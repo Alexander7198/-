01f7f954e73f2f7f966bd29e2775b068b9c2293c
Alexander Dorofeev <alexandedorofeev@yandex.ru>
2022-12-27 02:23:05 +0300

    90 / 27.12.22

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,8 +3,8 @@ import Preloader from '../../common/Preloader/Preloader';
 import s from './ProfileInfo.module.css';
 import ProfileStatusWithHooks from './ProfileStatusWithHooks';
 
-const ProfileInfo = (props) => {
-  if (! props.profile ) {
+const ProfileInfo = ({profile, status, updateStatus}) => {
+  if (!profile ) {
     return <Preloader />
   };
   
@@ -14,8 +14,8 @@ const ProfileInfo = (props) => {
         <img src='https://img1.akspic.ru/attachments/crops/4/8/5/4/34584/34584-mersedes_amg-avtomobil-mercedes_benz-superkar-sportivnyj_avtomobil-1920x1080.jpg' alt={""}></img>
       </div> */}
       <div className={s.descriptionBlock}>
-        <img src={props.profile.photos.large} alt={""} />
-        <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus} />
+        <img src={profile.photos.large} alt={""} />
+        <ProfileStatusWithHooks status={status} updateStatus={updateStatus} />
       </div>
     </div >
   )
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -12,7 +12,7 @@ let initialState = {
         { id: 3, message: 'Blabla', likesCount: 9 },
         { id: 4, message: 'Ditch', likesCount: 1 }
     ],
-    
+
     profile: null,
     status: ""
 };
@@ -31,7 +31,7 @@ const profileReducer = (state = initialState, action) => {
                 newPostText: '',
             };
         }
-        
+
         case SET_STATUS: {
             return {
                 ...state,
@@ -54,26 +54,21 @@ export const addPostActionCreator = (newPostText) => ({ type: ADD_POST, newPostT
 export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile })
 export const setStatus = (status) => ({ type: SET_STATUS, status })
 
-export const getUserProfile = (userId) => (dispatch) => {
-    usersAPI.getProfile(userId).then(response => {
-        dispatch(setUserProfile(response.data));
-      });
-} 
+export const getUserProfile = (userId) => async (dispatch) => {
+    const response = await usersAPI.getProfile(userId);
+    dispatch(setUserProfile(response.data));
+}
 
-export const getStatus = (userId) => (dispatch) => {
-    profileAPI.getStatus(userId)
-    .then(response => {
-        dispatch(setStatus(response.data));
-      });
-} 
+export const getStatus = (userId) => async (dispatch) => {
+    let response = await profileAPI.getStatus(userId);
+    dispatch(setStatus(response.data));
+}
 
-export const updateStatus = (status) => (dispatch) => {
-    profileAPI.updateStatus(status)
-    .then(response => {
-        if (response.data.resultCode === 0) {
-            dispatch(setStatus(status));
-        }
-      });
-} 
+export const updateStatus = (status) => async (dispatch) => {
+    let response = await profileAPI.updateStatus(status);
+    if (response.data.resultCode === 0) {
+        dispatch(setStatus(status));
+    }
+}
 
 export default profileReducer;
